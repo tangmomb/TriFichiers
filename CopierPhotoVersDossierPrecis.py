6c35c6106b4a398f38afc47b1a387a36644b561e
tangmomb <hello@hello.fr>
2025-09-12 18:13:31 +0200

    Test

--- CopierPhotoVersDossierPrecis.py
+++ CopierPhotoVersDossierPrecis.py
@@ -1,5 +1,7 @@
 #Permet de visualiser les images d'un dossier une par une, avec la possibilité de les copier dans un autre dossier en appuyant sur la touche ESPACE.
 
+#test
+
 import os
 import cv2
 import shutil
