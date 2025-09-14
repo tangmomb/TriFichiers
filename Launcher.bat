afedee52cc581b060580bc9ae181493f635a6137
tangmomb <hello@hello.fr>
2025-09-14 12:46:22 +0200

    Suppression fichiers des autres branches

---
+++ Launcher.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Récupérer le dossier où est lancé le .bat
+set BASE_DIR=%~dp0
+
+REM Supprimer le backslash final pour éviter les problèmes
+set BASE_DIR=%BASE_DIR:~0,-1%
+
+REM Activer l'environnement virtuel dans Scripts/
+call "%BASE_DIR%\venv\Scripts\activate"
+
+REM Lancer le script Python dans le même dossier
+python "%BASE_DIR%\Launcher.py"
+
+REM Laisser la console ouverte pour voir les inputs
+pause
